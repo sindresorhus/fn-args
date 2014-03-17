a9a7b2c067c6f87659f2362c6e5b1ea6bdcd04e4
Gilad Peleg <giladp007@gmail.com>
2014-03-17 12:06:32 +0100

    faster regex - closes #1

--- fn-args.js
+++ fn-args.js
@@ -8,20 +8,20 @@
 (function () {
 	'use strict';
 
-	var reFnArgs = /\(([^)]*)\)/;
+	var reFnArgs = /^function\s*[^(]*\(([^)]+)\)/;
 
 	var fnArgs = function (fn) {
 		if (typeof fn !== 'function') {
 			throw new TypeError('Expected a function');
 		}
-		
+
 		if (fn.length === 0) {
 			return [];
 		}
 
 		var match = reFnArgs.exec(fn.toString());
 
-		return match && match[1].length > 0 ? match[1].split(',').map(function (el) {
+		return match ? match[1].split(',').map(function (el) {
 			return el.trim();
 		}) : [];
 	};
