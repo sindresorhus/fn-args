45a2887f2d4a9f191cbe07649d84cd179232660b
GlaDos <glados28@yandex.ru>
2017-02-06 03:23:27 +0300

    upgraded code structure

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-"use strict";
+'use strict';
 
-/* FIXME: sorry for different code style. I used my config, moreover, still infringe some rules. Rewrite it as You wish */
-
-module.exports = (fn) => {
-	if (typeof fn !== "function") {
-		throw new TypeError("Expected a function");
+module.exports = fn => {
+	if (typeof fn !== 'function') {
+		throw new TypeError('Expected a function');
 	}
 
-	const
-		reComments = /(\/\*([\s\S]*?)\*\/|([^:]|^)\/\/(.*)$)/mg,
-		quotes     = ["`", "'", '"'];
+	const reComments = /(\/\*([\s\S]*?)\*\/|([^:]|^)\/\/(.*)$)/mg;
+	const quotes = ['`', '"', '\''];
 
-	const fnNC = fn.toString().replace(reComments, ""); /* fn with no comments */
+	const fnNC = fn.toString().replace(reComments, ''); /* fn with no comments */
 
-	let
-		depth = 0, /* () [] {} */
-		fnND = "",   /* fn with no default values */
-		i = 0;
+	let depth = 0; /* () [] {} */
+	let fnND = ''; /* fn with no default values */
+	let i = 0;
 
 	/*
 	 * to remove default values we can not use regexp because
@@ -24,19 +20,17 @@ module.exports = (fn) => {
 	 */
 
 	/* remove default values */
-	for (; i < fnNC.length && fnNC.charAt(i) !== ")"; i += 1) {
-
+	for (; i < fnNC.length && fnNC.charAt(i) !== ')'; i += 1) {
 		/* exiting if an arrow occurs (needed when arrow function without '()') */
-		if (fnNC.startsWith("=>", i)) {
+		if (fnNC.startsWith('=>', i)) {
 			fnND = fnNC;
 			i = fnNC.length;
 			break;
 		}
 
 		/* if we found a default value - skip it */
-		if (fnNC.charAt(i) === "=") {
-			for (; i < fnNC.length && (depth !== 0 || fnNC.charAt(i) !== "," && fnNC.charAt(i) !== ")"); i += 1) {
-
+		if (fnNC.charAt(i) === '=') {
+			for (; i < fnNC.length && ((fnNC.charAt(i) !== ',' && fnNC.charAt(i) !== ')') || depth !== 0); i += 1) {
 				/* skip all quotes */
 				let wasQuote = false;
 
@@ -44,7 +38,8 @@ module.exports = (fn) => {
 					if (fnNC.charAt(i) === quotes[j]) {
 						i += 1;
 
-						for (; i < fnNC.length && fnNC.charAt(i) !== quotes[j]; i += 1) {
+						for (; i < fnNC.length && fnNC.charAt(i) !== quotes[j];) {
+							i += 1;
 						}
 
 						wasQuote = true;
@@ -53,37 +48,41 @@ module.exports = (fn) => {
 				}
 
 				/* if any quote type was skipped, start the cycle again */
-				if (wasQuote)
+				if (wasQuote) {
 					continue;
+				}
 
 				switch (fnNC.charAt(i)) { /* keeps right depths of all types of parenthesises */
-				case "(":
-				case "[":
-				case "{":
-					depth += 1;
-					break;
-				case ")":
-				case "]":
-				case "}":
-					depth -= 1;
+					case '(':
+					case '[':
+					case '{':
+						depth += 1;
+						break;
+					case ')':
+					case ']':
+					case '}':
+						depth -= 1;
+						break;
+					default:
 				}
 			}
 
-			if (fnNC.charAt(i) === ",")
-				fnND += ",";
+			if (fnNC.charAt(i) === ',') {
+				fnND += ',';
+			}
 
-			if (fnNC.charAt(i) === ")") { /* quits from the cycle immediately */
-				fnND += ")";
+			if (fnNC.charAt(i) === ')') { /* quits from the cycle immediately */
+				fnND += ')';
 				break;
 			}
-		} else
+		} else {
 			fnND += fnNC.charAt(i);
+		}
 	}
 
-	if (i < fnNC.length && fnNC.charAt(i) === ")")
-		fnND += ")";
-
-	/* TODO: please, check regexp on having excess data: fnND already has not a function body and arrows */
+	if (i < fnNC.length && fnNC.charAt(i) === ')') {
+		fnND += ')';
+	}
 
 	// the first part matches parenthesises-less arrow functions
 	// the second part matches the rest
@@ -91,7 +90,5 @@ module.exports = (fn) => {
 
 	const match = reFnArgs.exec(fnND);
 
-	return match
-		? (match[1] || match[2]).split(",").map((el) => el.trim())
-		: [];
+	return match ? (match[1] || match[2]).split(',').map(el => el.trim()) : [];
 };
