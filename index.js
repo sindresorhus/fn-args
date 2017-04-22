1e830666098e5860d293ca1da8cc38f464cd0374
GlaDos28 <glados28@yandex.ru>
2017-04-22 15:48:29 +0400

    Support rest parameters & default values (#8)

--- index.js
+++ index.js
@@ -4,17 +4,90 @@ module.exports = fn => {
 		throw new TypeError('Expected a function');
 	}
 
-	if (fn.length === 0) {
-		return [];
+	const reComments = /(\/\*([\s\S]*?)\*\/|([^:]|^)\/\/(.*)$)/mg;
+	const quotes = ['`', '"', '\''];
+
+	const fnNC = fn.toString().replace(reComments, ''); /* fn with no comments */
+
+	let depth = 0; /* () [] {} */
+	let fnND = ''; /* fn with no default values */
+	let i = 0;
+
+	/*
+	 * to remove default values we can not use regexp because
+	 * finite automaton can not handle such things as (potential) infinity-nested blocks (), [], {}
+	 */
+
+	/* remove default values */
+	for (; i < fnNC.length && fnNC.charAt(i) !== ')'; i += 1) {
+		/* exiting if an arrow occurs (needed when arrow function without '()') */
+		if (fnNC.startsWith('=>', i)) {
+			fnND = fnNC;
+			i = fnNC.length;
+			break;
+		}
+
+		/* if we found a default value - skip it */
+		if (fnNC.charAt(i) === '=') {
+			for (; i < fnNC.length && ((fnNC.charAt(i) !== ',' && fnNC.charAt(i) !== ')') || depth !== 0); i += 1) {
+				/* skip all quotes */
+				let wasQuote = false;
+
+				for (let j = 0; j < quotes.length; j += 1) {
+					if (fnNC.charAt(i) === quotes[j]) {
+						i += 1;
+
+						for (; i < fnNC.length && fnNC.charAt(i) !== quotes[j];) {
+							i += 1;
+						}
+
+						wasQuote = true;
+						break;
+					}
+				}
+
+				/* if any quote type was skipped, start the cycle again */
+				if (wasQuote) {
+					continue;
+				}
+
+				switch (fnNC.charAt(i)) { /* keeps right depths of all types of parenthesises */
+					case '(':
+					case '[':
+					case '{':
+						depth += 1;
+						break;
+					case ')':
+					case ']':
+					case '}':
+						depth -= 1;
+						break;
+					default:
+				}
+			}
+
+			if (fnNC.charAt(i) === ',') {
+				fnND += ',';
+			}
+
+			if (fnNC.charAt(i) === ')') { /* quits from the cycle immediately */
+				fnND += ')';
+				break;
+			}
+		} else {
+			fnND += fnNC.charAt(i);
+		}
 	}
 
-	const reComments = /(\/\*([\s\S]*?)\*\/|([^:]|^)\/\/(.*)$)/mg;
+	if (i < fnNC.length && fnNC.charAt(i) === ')') {
+		fnND += ')';
+	}
 
 	// the first part matches parens-less arrow functions
 	// the second part matches the rest
 	const reFnArgs = /^(?:async)?([^=()]+)=|\(([^)]+)\)/;
 
-	const match = reFnArgs.exec(fn.toString().replace(reComments, ''));
+	const match = reFnArgs.exec(fnND);
 
 	return match ? (match[1] || match[2]).split(',').map(x => x.trim()) : [];
 };
